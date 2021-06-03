2ecfbb2581b1541a75b5efdbbe6db45d0aa540f2
Nikita <danik.fedorov.71@mail.ru>
2021-06-03 20:53:10 +0300

    Урок 7 Создал класс  HorizontalLine

---
+++ HorizontalLine.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+    }
+    public HorizontalLine(int xleft, int xRight, int y, char sym);
+}
+    pList = new List<Point>();
+    for (int x = xLeft; x <= xReight; x++)
+{
+    Point p = new Point(x, y, sym);
+    pList.Add(p);
+}
+
+                 
+                
+            
+
+
+
+
+
+
+   
