7394575635f17d65b9cd9e59862a51dcb300e7d8
opilane <opilane@HP-CZC6297BB7>
2021-06-03 12:08:34 +0300

    Cоздал классы, 3 урок

---
+++ Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public int sym;
+        
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
